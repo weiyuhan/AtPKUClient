2158d3062b5161539e463d7b164345203961571d
weiyuhan <1300012901@pku.edu.cn>
2016-05-22 15:18:40 +0800

    增加登录和注册的http请求，但是请求失败

--- app/src/main/java/atpku/client/window/LoginWindow.java
+++ app/src/main/java/atpku/client/window/LoginWindow.java
@@ -6,6 +6,7 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,16 +15,28 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import atpku.client.R;
 
 
 public class LoginWindow extends Activity
 {
-    public EditText username;
+    public EditText email;
     public EditText password;
     public Button loginButton;
     public Button registButton;
     public ActionBar actionBar;
+
+    private RequestQueue volleyQuque;
+
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -33,17 +46,45 @@ public class LoginWindow extends Activity
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
 
-        username = (EditText)findViewById(R.id.login_username);
+        email = (EditText)findViewById(R.id.login_email);
         password = (EditText)findViewById(R.id.login_password);
         loginButton = (Button)findViewById(R.id.loginButton);
         registButton = (Button)findViewById(R.id.registButton);
+
+        volleyQuque = Volley.newRequestQueue(this);
     }
 
     public void loginHandler(View source) //登录请求
     {
-        MapWindow.isLogin = true;
-        Toast.makeText(this, "登录成功", Toast.LENGTH_LONG).show();
-        finish();
+        StringRequest stringRequest = new StringRequest(StringRequest.Method.POST,"http://139.129.22.145:5000",
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        System.out.println("response : " + response);
+                        //Toast.makeText(LoginWindow.this, "it works!", Toast.LENGTH_LONG).show();
+                        Log.d("TAG", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //System.out.println("error : " + error.getMessage());
+                        Log.e("TAG", error.getMessage(), error);
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                //在这里设置需要post的参数
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("email", email.getText().toString());
+                params.put("password", password.getText().toString());
+                return params;
+            }
+        };
+        volleyQuque.add(stringRequest);
     }
     public void regisHandler(View source) //注册页面
     {
--- app/src/main/java/atpku/client/window/RegisterWindow.java
+++ app/src/main/java/atpku/client/window/RegisterWindow.java
@@ -4,11 +4,23 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import atpku.client.R;
 
@@ -21,15 +33,11 @@ public class RegisterWindow extends Activity
     public RadioButton male;
     public RadioButton female;
     public RadioButton secret_sex;
-    public static int GENDER_MALE = 0;
-    public static int GENDER_FEMALE = 1;
-    public static int GENDER_ORTHER = 2;
     public Button submitButton;
-    public Button backButton;
 
     public ActionBar actionBar;
 
-
+    private RequestQueue volleyQuque;
 
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -49,10 +57,80 @@ public class RegisterWindow extends Activity
         female = (RadioButton)findViewById(R.id.register_female);
         secret_sex = (RadioButton)findViewById(R.id.register_secret);
         submitButton = (Button)findViewById(R.id.regist_submitButton);
+
+        volleyQuque = Volley.newRequestQueue(this);
     }
 
     public void registSubmitHandler(View source)
     {
+        final String stuNumStr = studentNum.getText().toString();
+        if (stuNumStr.length() < 1)
+        {
+            Toast.makeText(this, "请输入学号", Toast.LENGTH_SHORT);
+            return;
+        }
+        final String userNameStr = username.getText().toString();
+        if (userNameStr.length() < 1)
+        {
+            Toast.makeText(this, "请输入用户名", Toast.LENGTH_SHORT);
+            return;
+        }
+        final String passwordStr = password.getText().toString();
+        if (passwordStr.length() < 1)
+        {
+            Toast.makeText(this, "请输入密码", Toast.LENGTH_SHORT);
+            return;
+        }
+        String confirmpwStr = confirmPasswd.getText().toString();
+        if (passwordStr.length() < 1)
+        {
+            Toast.makeText(this, "请确认密码", Toast.LENGTH_SHORT);
+            return;
+        }
+        else if(!confirmpwStr.equals(passwordStr))
+        {
+            Toast.makeText(this, "两次输入密码不一致", Toast.LENGTH_LONG);
+            return;
+        }
+        String genderStrTmp = null;
+        if(male.isActivated())
+            genderStrTmp = "m";
+        else
+            genderStrTmp = "f";
+        final String genderStr = genderStrTmp;
+
+        StringRequest stringRequest = new StringRequest(StringRequest.Method.POST,"http://139.129.22.145:5000",
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        System.out.println("response : " + response);
+                        //Toast.makeText(RegisterWindow.this, "it works!", Toast.LENGTH_LONG).show();
+                        Log.d("TAG", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //System.out.println("error : " + error.getMessage());
+                        Log.e("TAG", error.getMessage(), error);
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() {
+                //在这里设置需要post的参数
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("nickname", userNameStr);
+                params.put("email", stuNumStr + ".pku.edu.cn");
+                params.put("password", passwordStr);
+                params.put("gender", genderStr);
+                return params;
+            }
+        };
+        volleyQuque.add(stringRequest);
+
 
     }
 
